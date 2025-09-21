5d5052042f3e1a3d9ef0d7e1ab036bed113c5f4c
raj <rm07870@gmail.com>
2025-09-21 10:21:54 +0530

    service key

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-cred = credentials.Certificate('path/to/serviceAccountKey.json')
+cred = credentials.Certificate('serviceAccountKey.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
